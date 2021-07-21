111f55c573abd39c7dc1c382c72353f0d1e5e7fd
TheMadFitz <hayden.fitzgerald@outlook.com.au>
2021-07-21 19:03:40 +1000

    Updated with basic command handler - comments

--- MessageHandler.cs
+++ MessageHandler.cs
@@ -10,20 +10,22 @@ using Microsoft.Extensions.Configuration.Json;
 
 namespace testboy
 {
-    public static class MessageHandler
+    public static class MessageHandler // Static means we don't have to create an instance of the class - we can just use it :)
     {
-        public static async void HandleMessage(SocketMessage message)
+        public static async void HandleMessage(SocketMessage message) // We can use MessageHandler.HandleMessage(message) to deal with any incoming messages and handle them here.
         {
 
-            if (message.Content == ".hello")
+            if (message.Content == ".hello")  //If statements are a poor choice for this kind of work - We'll work on making this better in our next session.
             {
-                await message.Channel.SendMessageAsync("World!");
+                await message.Channel.SendMessageAsync("World!"); //Our original command
             }
             else if (message.Content == ".NewCmd")
             {
-                await message.Channel.SendMessageAsync("This is the new command");
+                await message.Channel.SendMessageAsync("This is the new command"); // Our new command
 
             }
+
+            // your mission - Using what you can see - add some more commands and responses.
         }
 
 
